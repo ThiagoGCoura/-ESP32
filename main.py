5c747687cd7cfc9c9a85d1cd941c02ae654688d2
Thiago Gonçalves Coura <thiagocoura@gmail.com>
2022-11-21 12:08:08 -0400

    2° Atualização

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-"""
-MicroPython IoT Weather Station Example for Wokwi.com
-
-To view the data:
-
-1. Go to http://www.hivemq.com/demos/websocket-client/
-2. Click "Connect"
-3. Under Subscriptions, click "Add New Topic Subscription"
-4. In the Topic field, type "wokwi-weather" then click "Subscribe"
-
-Now click on the DHT22 sensor in the simulation,
-change the temperature/humidity, and you should see
-the message appear on the MQTT Broker, in the "Messages" pane.
-
-Copyright (C) 2022, Uri Shaked
-
-https://wokwi.com/arduino/projects/322577683855704658
-"""
- 
 import network
 import time
 from machine import Pin
@@ -24,23 +5,16 @@ import dht
 import ujson
 from umqtt.simple import MQTTClient
 
-from time import sleep # led
-
 # MQTT Server Parameters
-MQTT_CLIENT_ID = "clientId-4l32XFaB5h"
+MQTT_CLIENT_ID = "micropython-weather-demo"
 MQTT_BROKER    = "broker.mqttdashboard.com"
 MQTT_USER      = ""
 MQTT_PASSWORD  = ""
-MQTT_TOPIC     = "wokwi-weather-27"
+MQTT_TOPIC     = "10920515391-ThiagoGCoura"
 
-# Sensor is conected to pin 15
 sensor = dht.DHT22(Pin(15))
-
-#LED conected to D2 as output
 led = Pin(2,Pin.OUT)
-
-#Button is conected to pin 13
-push_button = Pin(13, Pin.IN)
+push_button = Pin(13,Pin.IN)
 
 print("Connecting to WiFi", end="")
 sta_if = network.WLAN(network.STA_IF)
@@ -74,10 +48,8 @@ while True:
     print("No change")
   time.sleep(1)
 
-  # while led
   logic_state = push_button.value()
-  if logic_state == True:  # We press the button
-    led.value(1) # LA LED s’allume
-  else: # We release the button
-    led.value(0) #LED goes out
-
+  if logic_state == True:
+    led.value(1)
+  else:
+    led.value(0)
